6a98bc00b648452cea0036d1e70cc3d967132ae6
jadenzq <jadenzq@gmail.com>
2025-07-07 16:16:07 +0800

    Updated Team component to use User from database

---
+++ project-management-app/src/Utils/Utils.js
@@ -0,0 +1,14 @@
+export const capitalizeEachWord = (str) => {
+  const words = str.split(" ");
+  const capitalizedStr = words.map(word => {
+    if(word.length === 0){
+      return "";
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return capitalizedStr.join(" ");
+}
+
+export const removeHyphen = (str) => {
+  return str.replace("-", " ");
+}
